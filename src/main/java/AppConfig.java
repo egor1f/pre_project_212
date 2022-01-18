91d0a9f2a073118c5b364710683088c12c080606
Bolotov Alexander <distalker1@gmail.com>
2022-01-18 08:42:56 +0300

    fix pom

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -11,4 +11,13 @@ public class AppConfig {
         helloWorld.setMessage("Hello World!");
         return helloWorld;
     }
+
+    @Bean(name = "cat")
+    @Scope("prototype")
+    public Cat getCatName() {
+        Cat cat = new Cat();
+        cat.setName("Cat name: Barsik");
+        return cat;
+    }
+
 }
